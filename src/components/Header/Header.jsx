c80bec6c5142e0179138666524bf00738fbe2689
Timur Gorbakov <gerodot050695@gmail.com>
2025-02-05 23:31:58 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logoSvg from "../../assets/img/pizza-logo.svg";
 import { Link } from "react-router-dom";
+import Search from "../Search/Search";
 function Header() {
     return (
         <div className="header">
@@ -9,11 +10,12 @@ function Header() {
                     <div className="header__logo">
                         <img width="38" src={logoSvg} alt="Pizza logo" />
                         <div>
-                            <h1>React Pizza</h1>
+                            <h1>React Pizza V2</h1>
                             <p>самая вкусная пицца во вселенной</p>
                         </div>
                     </div>
                 </Link>
+                <Search />
                 <div className="header__cart">
                     <Link to="/cart" className="button button--cart">
                         <span>520 ₽</span>
